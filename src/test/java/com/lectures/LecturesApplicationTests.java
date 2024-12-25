b68882185f1846db339882f796de98f687047c7d
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2024-12-25 15:14:04 +0900

    Initial commit

---
+++ src/test/java/com/lectures/LecturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lectures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
